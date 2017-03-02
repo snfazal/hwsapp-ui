8360ff1d8f6c295193d2c23773790d5cfc0150bf
snfazal <snfazal@gmail.com>
2017-03-02 12:27:12 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express        = require('express');
+var logger         = require('morgan');
+
+var app = express();
+
+app.use(express.static('public'))
+
+app.use(logger('dev'));
+
+app.listen(process.env.PORT || 4000, function() {
+  console.log(`listening on 4000`)
+});
